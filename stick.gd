9e7e9c430f29260704749a2d1b3195363024b2f7
unknown <solojkim@gmail.com>
2024-01-08 23:39:56 -0500

    basic invincibility

--- stick.gd
+++ stick.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-var health = 3
-
+var health = 1
+signal health_depleted
+var is_invincible = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -10,8 +11,21 @@ func _process(delta):
 	move_and_slide()
 	rotate(0.015)
 
+	
+	var damage_contact_left = %HurtBoxLeft.get_overlapping_bodies()
+	var damage_contact_right = %HurtBoxRight.get_overlapping_bodies()
+	
+	if (damage_contact_left.size() > 0 || damage_contact_right.size() > 0) && is_invincible == false: 
+		health -= 10
+		set_rotation_degrees(-15)
+		start_invincibility()
+		if health == 0:
+			health_depleted.emit()
 
-func take_damage():
-	health -= 1
-	if health == 0:
-		queue_free()
+
+func _on_invincibility_timer_timeout():
+	is_invincible = false
+
+func start_invincibility():
+	is_invincible = true
+	%Invincibility_Timer.start()
