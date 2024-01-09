2c7e6b69a25d6ce069f4473179e0ea823038c76c
unknown <solojkim@gmail.com>
2024-01-08 22:10:25 -0500

    base stuff WITH IGNORE

---
+++ stick.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * 600
+	move_and_slide()
+	rotate(0.015)
+
+
+
