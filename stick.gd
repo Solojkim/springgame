ae97cf1523236bc4cdf9b7bff7668b7182daeef7
unknown <solojkim@gmail.com>
2024-01-09 20:52:31 -0500

    spring scene

--- stick.gd
+++ stick.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 var health = 5
+var rotation_speed = 0.015
+
 signal health_depleted
 signal win_zone
 
@@ -14,7 +16,7 @@ func _process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
-	rotate(0.015)
+	rotate(rotation_speed)
 	
 
 	var damage_contact_left = %HurtBoxLeft.get_overlapping_bodies()
@@ -36,6 +38,10 @@ func knockback(delta):
 	var current_rotation_degrees: float = rotation_degrees
 	var new_rotation_degrees: float = lerp(current_rotation_degrees, current_rotation_degrees - 35, 105 * delta)
 	rotation_degrees=new_rotation_degrees
+
+
+func reverse_rotation():
+	rotation_speed = rotation_speed*-1
 
 
 func _on_invincibility_timer_timeout():
